3129abf282941d5e4c2fa48e6ccfb33c68463a8e
TanviSurve0005 <tanvisurve1805@gmail.com>
2026-06-22 01:05:47 +0530

    Refactor Basic.js to include arithmetic functions and create math.js to demonstrate module exports

--- basic.js
+++ basic.js
@@ -3,3 +3,27 @@ console.log("Hello world")
 setTimeout(() => {
     console.log("Hii Ram")
 }, 10000)
+
+
+
+
+function add(a,b) {
+    return a+b
+}
+
+function subtract(a,b) {
+    return a-b
+}
+
+console.log(add(5,6))
+
+module.exports = {
+    addfnc: add,
+    subfnc: subtract,
+};
+
+exports.mul= (a,b)=>{
+    return a*b
+};
+
+exports.div = (a,b) => a/b;
---
+++ math.js
@@ -0,0 +1,5 @@
+const math = require("./Basic");
+
+// // `Basic.js` exports { addfnc, subfnc }
+// console.log(math.addfnc(5, 6), math.subfnc(6,5))
+console.log(math)
