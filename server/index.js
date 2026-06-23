125109d107b83617c09b1f5ec3644ec4183ee66b
TanviSurve0005 <tanvisurve1805@gmail.com>
2026-06-24 02:17:41 +0530

    Add server functionality with request logging and basic routing

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const fs = require("fs")
+const myServer = http.createServer((req, res) =>{
+    const log = `${Date.now()}: ${req.url} New Request receive\n`
+    fs.appendFile('./log.txt', log, (err, Data) => {
+        switch(req.url){
+            case'/': res.end("HomePage");
+            break
+            case'/about': res.end("AboutPage");
+            break
+            default: res.end("404 not found") 
+
+        }
+    })
+    
+});
+
+
+myServer.listen(8000, () => console.log("Server Started"))
